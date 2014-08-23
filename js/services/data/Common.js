4455b7775a29d99c7f1aaeea06eabae15f7053bc
Jovan <jtomasevic@gmail.com>
2014-08-23 21:06:36 +0200

    change api base url

--- js/services/data/Common.js
+++ js/services/data/Common.js
@@ -2,8 +2,8 @@
  * Created by jtomasevic on 8/23/14.
  */
 
-var _is_live_env
-var _apiBaseUrl = (_is_live_env) ? "/www/chisel/data/story1.js" : "/data/story1.js"
+var _is_live_env = false
+var _apiBaseUrl = (_is_live_env) ? "/www/chisel/data/story1.js" : "http://localhost:47076/api/"
 function _getApiUrl(method){
     return _apiBaseUrl + method;
 }
