345290879d4f099bb3786e1da4a42bbdccb76aec
Jovan <jtomasevic@gmail.com>
2014-09-07 01:15:12 +0200

    debug and live/develop configuration

--- js/services/data/Common.js
+++ js/services/data/Common.js
@@ -2,7 +2,8 @@
  * Created by jtomasevic on 8/23/14.
  */
 
-var _is_live_env = false
+var _is_live_env = false;
+var is_test_env = true;
 var _apiBaseUrl = (_is_live_env) ? "http://ticatjerryapi.azurewebsites.net/api/" : "http://localhost:47076/api/"
 function _getApiUrl(method){
     return _apiBaseUrl + method;
